c8b6790e29d481caf3ccdcdc38a70752ad84bc0e
BingoChado <nicolas.p.ammann@gmail.com>
2022-06-08 16:12:43 -0600

    Made modules public

--- grid/src/lib.rs
+++ grid/src/lib.rs
@@ -3,7 +3,7 @@ This library implements the GRID protocol. It should eventually have C bindings
 but that will be down the road a little bit
 */
 
-mod client;
-mod definitions;
-mod server;
+pub mod client;
+pub mod definitions;
+pub mod server;
 
